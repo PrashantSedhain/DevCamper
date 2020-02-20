56a9cc2b15ac91efb6de9c267f7e41371cdb8db3
Prashant Sedhain <prashantased@gmail.com>
2020-02-19 23:33:25 -0600

    added db.js

---
+++ config/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+NONGO_URI = mongodb+srv://prashant:prashant123@cluster0-tmtmt.mongodb.net/test?retryWrites=true&w=majority
+const connectDB = async () => {
+    const conn =  mongoose.connect()
+}
--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 express = require("express");
 dotenv = require("dotenv");
-const logger = require('./Middleware/logger')
-const morgan = require('morgan')
+const logger = require("./Middleware/logger");
+const morgan = require("morgan");
 //Route Files
-const bootcamps = require('./routes/bootcamps');
+const bootcamps = require("./routes/bootcamps");
 //load env vars
-dotenv.config({path: './config/config.env'});
-
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 //Dev logging middleware
-
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(logger);
 
 //Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `App listening on port ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
 });
-
